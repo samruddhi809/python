fae05c505f7635bf05124ea522a7e8fa651261fc
samruddhi809 <127029171+samruddhi809@users.noreply.github.com>
2024-05-07 13:59:06 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,3 +1,13 @@
 print("Hello ")
 
 print(" hyy  I am from DYP Varale")
+
+# Python3 program to add two numbers
+num1 = 15
+num2 = 12
+ 
+# Adding two nos
+sum = num1 + num2
+ 
+# printing values
+print("Sum of", num1, "and", num2 , "is", sum)
